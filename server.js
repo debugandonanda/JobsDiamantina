754e8f471e7a2c38f0612d0952306c21a242c0c4
Nanda Dantas <nandaadantas@gmail.com>
2022-08-14 23:52:19 -0300

    pages

---
+++ server.js
@@ -0,0 +1,17 @@
+// 1. chamar uma instância do router
+var adminRouter = express.Router()
+
+// 2. criar rotas para ele
+// página principal (http://localhost:3000/home)
+adminRouter.get('/', (req, res) => {
+  res.send("index.html")
+})
+
+// página de vaga (http://localhost:3000/home/vagas)
+adminRouter.get('/users', (req, res) => {
+  res.send('vaga.html')
+})
+
+
+// 3. adicionar essas rotas a aplicação principal
+app.use('/home', adminRouter)
